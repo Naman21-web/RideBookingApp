06a005172667b7d21796c0d8bd940d891fc9aadc
Naman <jainn0984@gmail.com>
2026-03-23 20:13:52 +0530

    Added logging details for login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,11 +2,17 @@ import { Request, Response } from 'express';
 import * as authService from '../services/auth.service';
 import { asyncHandler } from '../utils/asyncHandler';
 import { successResponse } from '../utils/apiResponse';
+import logger from '../utils/logger';
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   const result = await authService.loginUser(email, password);
+
+  logger.info('User logged in', {
+            userId: result.user.id,
+            email: result.user.email,
+        });
 
   return successResponse(res, result, 'Login successful');
 });
