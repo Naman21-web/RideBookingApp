062d5520207b7b43b35ae2b2200d18f837a2c51f
Naman <jainn0984@gmail.com>
2026-03-26 13:46:25 +0530

    Added distance & fare calc logic

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const STATUS_CODES = {
     OK: 200,
     CREATED: 201,
+    NO_CONTENT: 204,
     BAD_REQUEST: 400,
     UNAUTHORISED: 401,
     PAYMENT_REQUIRED: 402,
@@ -11,3 +12,11 @@ export const STATUS_CODES = {
     UNPROCESSABLE: 422,
     INTERNAL_SERVER_ERROR: 500
 }
+
+export const PRICING = {
+  RICKSHAW: { baseFare: 30, perKm: 10 },
+  HATCHBACK: { baseFare: 50, perKm: 12 },
+  SEDAN: { baseFare: 70, perKm: 15 },
+  SUV: { baseFare: 100, perKm: 18 },
+  SEVEN_SEATER: { baseFare: 120, perKm: 20 },
+};
